3c79629c14352d63a38e4b0578353a8875041965
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-08-07 22:42:43 +0530

    Add comments for employee and department fetching functions

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
 const BASE_URL = "https://api.freeprojectapi.com/api/EmployeeApp";
 
+//--------------get employees----------------//
+
 export async function getEmployees() {
     
     const response = await fetch(`${BASE_URL}/GetEmployees`);
@@ -10,5 +12,20 @@ export async function getEmployees() {
 
     return await response.json();
 
+}
+
+//-----------get department-----------------//
+
+export async function  getDepartments() {
+
+    const response = await fetch(`${BASE_URL}/GetDepartments`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch departments");
+        
+    }
+
+    return await response.json();
+    
 }
 
